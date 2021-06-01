41de5c4814c9462953e0c4dea11d6b04f5c500e4
铁建文 <jianwen_t@163.com>
2021-06-01 17:17:17 +0800

    test test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import "./styles.css";
+import "./styles.scss";
 
-export default function App() {
+function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      <div>aa</div>
     </div>
   );
 }
+
+export default App;
